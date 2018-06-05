f70b6dbd92ea139cf961c5a995ddb4b1ead42f7e
nida98 <39944769+nida98@users.noreply.github.com>
2018-06-05 15:26:23 +0530

    sum added to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 class Hello{
    public static void main(String[] args){
       system.out.println("Hello");
+      int a=100;
+      int b=100;
+      int sum=a+b;
+      system.out.println(String.valueof(sum));
       }
 }
